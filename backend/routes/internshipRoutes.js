f019501f44e711fd431681e8c3ba05e57df16f10
Ashish Singh <103932035+ashuisashish@users.noreply.github.com>
2025-04-26 02:35:22 +0530

    Delete backend directory

--- backend/routes/internshipRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { createInternship, getAllInternships } from '../controllers/internshipController.js';
-
-const router = express.Router();
-
-router.post('/create', createInternship);
-router.get('/all', getAllInternships);
-
-export default router;
